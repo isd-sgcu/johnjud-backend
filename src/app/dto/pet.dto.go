099ede4a16615dca63aef4b93f227afce2df33a7
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-21 00:18:12 +0700

    feat: add image to petDto

--- src/app/dto/pet.dto.go
+++ src/app/dto/pet.dto.go
@@ -5,19 +5,20 @@ import (
 )
 
 type PetDto struct {
-	Type       string     `json:"type" validate:"required"`
-	Species    string     `json:"species" validate:"required"`
-	Name       string     `json:"name" validate:"required"`
-	Birthdate  string     `json:"birthdate" validate:"required"`
-	Gender     pet.Gender `json:"gender" validate:"required" example:"male"`
-	Sterile    bool       `json:"sterile" validate:"required"`
-	Vaccine    bool       `json:"vaccine" validate:"required"`
-	Status     pet.Status `json:"status" validate:"required" example:"findhome"`
-	Habit      string     `json:"habit" validate:"required"`
-	Caption    string     `json:"caption"`
-	Visible    bool       `json:"visible" validate:"required"`
-	IsClubPet  bool       `json:"is_club_pet" validate:"required"`
-	Background string     `json:"background"`
-	Address    string     `json:"address"`
-	Contact    string     `json:"contact"`
+	Type         string     `json:"type" validate:"required"`
+	Species      string     `json:"species" validate:"required"`
+	Name         string     `json:"name" validate:"required"`
+	Birthdate    string     `json:"birthdate" validate:"required"`
+	Gender       pet.Gender `json:"gender" validate:"required" example:"male"`
+	Habit        string     `json:"habit" validate:"required"`
+	Caption      string     `json:"caption"`
+	Status       pet.Status `json:"status" validate:"required" example:"findhome"`
+	Image        []byte     `json:"image" validate:"required"`
+	IsSterile    bool       `json:"is_sterile" validate:"required"`
+	IsVaccinated bool       `json:"is_vaccine" validate:"required"`
+	IsVisible    bool       `json:"is_visible" validate:"required"`
+	IsClubPet    bool       `json:"is_club_pet" validate:"required"`
+	Background   string     `json:"background"`
+	Address      string     `json:"address"`
+	Contact      string     `json:"contact"`
 }
