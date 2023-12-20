e5cd0b5f696ce804a56bdceafb24536adbd2a03a
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-20 16:55:37 +0700

    feat: router context

---
+++ src/app/router/context.go
@@ -0,0 +1,78 @@
+package router
+
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+type FiberCtx struct {
+	*fiber.Ctx
+}
+
+func NewFiberCtx(c *fiber.Ctx) *FiberCtx {
+	return &FiberCtx{c}
+}
+
+func (c *FiberCtx) UserID() string {
+	return c.Ctx.Locals("UserId").(string)
+}
+
+func (c *FiberCtx) Bind(v interface{}) error {
+	return c.Ctx.BodyParser(v)
+}
+
+func (c *FiberCtx) JSON(statusCode int, v interface{}) {
+	c.Ctx.Status(statusCode).JSON(v)
+}
+
+func (c *FiberCtx) ID() (id string, err error) {
+	id = c.Params("id")
+
+	_, err = uuid.Parse(id)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func (c *FiberCtx) Param(key string) (value string, err error) {
+	value = c.Params(key)
+
+	if key == "id" {
+		_, err = uuid.Parse(value)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return value, nil
+}
+
+func (c *FiberCtx) Token() string {
+	raw := c.Ctx.Get(fiber.HeaderAuthorization, "")
+	parts := strings.Split(raw, " ")
+
+	if len(parts) > 1 {
+		return parts[1]
+	}
+	return ""
+}
+
+func (c *FiberCtx) Method() string {
+	return c.Ctx.Method()
+}
+
+func (c *FiberCtx) Path() string {
+	return c.Ctx.Path()
+}
+
+func (c *FiberCtx) StoreValue(k string, v string) {
+	c.Locals(k, v)
+}
+
+func (c *FiberCtx) Next() {
+	c.Ctx.Next()
+}
