32f8357ac90324974241007437b55476a0d0b96b
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-21 13:35:10 +0700

    feat: adopt, like handler skeleton

---
+++ src/app/handler/like/like.handler.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"github.com/isd-sgcu/johnjud-gateway/src/app/dto"
+	"github.com/isd-sgcu/johnjud-gateway/src/app/router"
+	"github.com/isd-sgcu/johnjud-gateway/src/app/validator"
+	proto "github.com/isd-sgcu/johnjud-go-proto/johnjud/backend/like/v1"
+)
+
+type Handler struct {
+	service  Service
+	validate *validator.DtoValidator
+}
+
+type Service interface {
+	FindAll() ([]*proto.Like, *dto.ResponseErr)
+	Create(*dto.LikeDto) (*proto.Like, *dto.ResponseErr)
+	Delete(string) (bool, *dto.ResponseErr)
+}
+
+func NewHandler(service Service, validate *validator.DtoValidator) *Handler {
+	return &Handler{service, validate}
+}
+
+func (h *Handler) FindAll(c *router.FiberCtx) {
+
+}
+
+func (h *Handler) Create(c *router.FiberCtx) {
+
+}
+
+func (h *Handler) Delete(c *router.FiberCtx) {
+
+}
