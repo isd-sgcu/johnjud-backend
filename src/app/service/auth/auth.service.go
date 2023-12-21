a45ae142e157b9bd9a9f5af13cbab107ffcb87b5
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-21 13:48:30 +0700

    feat: auth service skeleton

---
+++ src/app/service/auth/auth.service.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"github.com/isd-sgcu/johnjud-gateway/src/app/dto"
+	auth_proto "github.com/isd-sgcu/johnjud-go-proto/johnjud/auth/auth/v1"
+)
+
+type Service struct {
+	client auth_proto.AuthServiceClient
+}
+
+func NewService(client auth_proto.AuthServiceClient) *Service {
+	return &Service{
+		client: client,
+	}
+}
+
+func (s *Service) Signup(signup *dto.Signup) (*auth_proto.Credential, *dto.ResponseErr) {
+	return nil, nil
+}
+
+func (s *Service) Signin(signin *dto.Signin) (*auth_proto.Credential, *dto.ResponseErr) {
+	return nil, nil
+}
+
+func (s *Service) Signout(token string) (bool, *dto.ResponseErr) {
+	return false, nil
+}
+
+func (s *Service) Validate(token string) (*dto.TokenPayloadAuth, *dto.ResponseErr) {
+	return nil, nil
+}
+
+func (s *Service) RefreshToken(token string) (*auth_proto.Credential, *dto.ResponseErr) {
+	return nil, nil
+}
