0f5612160ab4a6a3fd36f6c2851781bde60dca37
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-21 14:08:21 +0700

    feat: image service skeleton

---
+++ src/app/service/image/image.service.go
@@ -0,0 +1,28 @@
+package image
+
+import (
+	"github.com/isd-sgcu/johnjud-gateway/src/app/dto"
+	proto "github.com/isd-sgcu/johnjud-go-proto/johnjud/file/image/v1"
+)
+
+type Service struct {
+	client proto.ImageServiceClient
+}
+
+func NewService(client proto.ImageServiceClient) *Service {
+	return &Service{
+		client: client,
+	}
+}
+
+func (s *Service) FindByPetId() ([]*proto.Image, *dto.ResponseErr) {
+	return nil, nil
+}
+
+func (s *Service) Upload(in *dto.ImageDto) (*proto.Image, *dto.ResponseErr) {
+	return nil, nil
+}
+
+func (s *Service) Delete(id string) (bool, *dto.ResponseErr) {
+	return false, nil
+}
