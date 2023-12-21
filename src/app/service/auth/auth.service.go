5d464610e710b065eddb64ecfd124d2306132004
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-21 13:49:55 +0700

    fix: auth service add userClient

--- src/app/service/auth/auth.service.go
+++ src/app/service/auth/auth.service.go
@@ -3,15 +3,18 @@ package auth
 import (
 	"github.com/isd-sgcu/johnjud-gateway/src/app/dto"
 	auth_proto "github.com/isd-sgcu/johnjud-go-proto/johnjud/auth/auth/v1"
+	user_proto "github.com/isd-sgcu/johnjud-go-proto/johnjud/auth/user/v1"
 )
 
 type Service struct {
-	client auth_proto.AuthServiceClient
+	client     auth_proto.AuthServiceClient
+	userClient user_proto.UserServiceClient
 }
 
-func NewService(client auth_proto.AuthServiceClient) *Service {
+func NewService(client auth_proto.AuthServiceClient, userClient user_proto.UserServiceClient) *Service {
 	return &Service{
-		client: client,
+		client:     client,
+		userClient: userClient,
 	}
 }
 
