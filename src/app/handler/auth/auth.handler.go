9a2cbfb52366356f84946a25618262a3a3b07c28
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-25 17:59:00 +0700

    feat: implement signup handler

--- src/app/handler/auth/auth.handler.go
+++ src/app/handler/auth/auth.handler.go
@@ -1,10 +1,14 @@
 package auth
 
 import (
+	"github.com/isd-sgcu/johnjud-gateway/src/app/constant"
+	"github.com/isd-sgcu/johnjud-gateway/src/app/dto"
 	"github.com/isd-sgcu/johnjud-gateway/src/app/router"
 	"github.com/isd-sgcu/johnjud-gateway/src/app/validator"
 	"github.com/isd-sgcu/johnjud-gateway/src/pkg/service/auth"
 	"github.com/isd-sgcu/johnjud-gateway/src/pkg/service/user"
+	"net/http"
+	"strings"
 )
 
 type Handler struct {
@@ -26,9 +30,37 @@ func (h *Handler) RefreshToken(c router.IContext) {
 }
 
 func (h *Handler) Signup(c router.IContext) {
-	// bind request
-	// validate request
-	// call authService.Signup
+	request := &dto.SignupRequest{}
+	err := c.Bind(request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, dto.ResponseErr{
+			StatusCode: http.StatusBadRequest,
+			Message:    constant.BindingRequestErrorMessage + err.Error(),
+			Data:       nil,
+		})
+		return
+	}
+
+	if err := h.validate.Validate(request); err != nil {
+		var errorMessage []string
+		for _, reqErr := range err {
+			errorMessage = append(errorMessage, reqErr.Message)
+		}
+		c.JSON(http.StatusBadRequest, dto.ResponseErr{
+			StatusCode: http.StatusBadRequest,
+			Message:    constant.InvalidRequestBodyMessage + strings.Join(errorMessage, ", "),
+			Data:       nil,
+		})
+		return
+	}
+
+	response, respErr := h.service.Signup(request)
+	if respErr != nil {
+		c.JSON(respErr.StatusCode, respErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *Handler) Signin(c router.IContext) {
