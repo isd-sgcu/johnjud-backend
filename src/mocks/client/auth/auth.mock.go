6be4d66b047039820055bdb6e2604697112fdb0e
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-25 15:36:39 +0700

    fix: AuthClientMock

--- src/mocks/client/auth/auth.mock.go
+++ src/mocks/client/auth/auth.mock.go
@@ -14,27 +14,27 @@ type AuthClientMock struct {
 func (m *AuthClientMock) Validate(_ context.Context, in *authProto.ValidateRequest, _ ...grpc.CallOption) (*authProto.ValidateResponse, error) {
 	args := m.Called(in)
 	resp, _ := args.Get(0).(*authProto.ValidateResponse)
-	err, _ := args.Get(1).(*error)
-	return resp, *err
+	err, _ := args.Get(1).(error)
+	return resp, err
 }
 
 func (m *AuthClientMock) RefreshToken(_ context.Context, in *authProto.RefreshTokenRequest, _ ...grpc.CallOption) (*authProto.RefreshTokenResponse, error) {
 	args := m.Called(in)
 	resp, _ := args.Get(0).(*authProto.RefreshTokenResponse)
-	err, _ := args.Get(1).(*error)
-	return resp, *err
+	err, _ := args.Get(1).(error)
+	return resp, err
 }
 
 func (m *AuthClientMock) Signup(_ context.Context, in *authProto.SignupRequest, _ ...grpc.CallOption) (*authProto.SignupResponse, error) {
 	args := m.Called(in)
 	resp, _ := args.Get(0).(*authProto.SignupResponse)
-	err, _ := args.Get(1).(*error)
-	return resp, *err
+	err, _ := args.Get(1).(error)
+	return resp, err
 }
 
 func (m *AuthClientMock) SignIn(_ context.Context, in *authProto.SignInRequest, _ ...grpc.CallOption) (*authProto.SignInResponse, error) {
 	args := m.Called(in)
 	resp, _ := args.Get(0).(*authProto.SignInResponse)
-	err, _ := args.Get(1).(*error)
-	return resp, *err
+	err, _ := args.Get(1).(error)
+	return resp, err
 }
