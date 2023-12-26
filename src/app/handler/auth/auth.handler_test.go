21746782896b494f117616f3e2eb4fbc845f571c
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-26 14:07:23 +0700

    chore: remove duplicate code

--- src/app/handler/auth/auth.handler_test.go
+++ src/app/handler/auth/auth.handler_test.go
@@ -17,6 +17,9 @@ import (
 
 type AuthHandlerTest struct {
 	suite.Suite
+	signupRequest *dto.SignupRequest
+	bindErr       error
+	validateErr   []*dto.BadReqErrResponse
 }
 
 func TestAuthHandler(t *testing.T) {
@@ -24,11 +27,27 @@ func TestAuthHandler(t *testing.T) {
 }
 
 func (t *AuthHandlerTest) SetupTest() {
+	signupRequest := &dto.SignupRequest{}
+	bindErr := errors.New("Binding request failed")
+	validateErr := []*dto.BadReqErrResponse{
+		{
+			Message:     "BadRequestError1",
+			FailedField: "Field1",
+			Value:       nil,
+		},
+		{
+			Message:     "BadRequestError2",
+			FailedField: "Field2",
+			Value:       nil,
+		},
+	}
 
+	t.signupRequest = signupRequest
+	t.bindErr = bindErr
+	t.validateErr = validateErr
 }
 
 func (t *AuthHandlerTest) TestSignupSuccess() {
-	signupRequest := &dto.SignupRequest{}
 	signupResponse := &dto.SignupResponse{
 		Id:        faker.UUIDDigit(),
 		Email:     faker.Email(),
@@ -42,9 +61,9 @@ func (t *AuthHandlerTest) TestSignupSuccess() {
 	validator := mock_validator.NewMockIDtoValidator(controller)
 	context := mock_router.NewMockIContext(controller)
 
-	context.EXPECT().Bind(signupRequest).Return(nil)
-	validator.EXPECT().Validate(signupRequest).Return(nil)
-	authSvc.EXPECT().Signup(signupRequest).Return(signupResponse, nil)
+	context.EXPECT().Bind(t.signupRequest).Return(nil)
+	validator.EXPECT().Validate(t.signupRequest).Return(nil)
+	authSvc.EXPECT().Signup(t.signupRequest).Return(signupResponse, nil)
 	context.EXPECT().JSON(http.StatusOK, signupResponse)
 
 	handler := NewHandler(authSvc, userSvc, validator)
@@ -53,11 +72,9 @@ func (t *AuthHandlerTest) TestSignupSuccess() {
 }
 
 func (t *AuthHandlerTest) TestSignupBindFailed() {
-	signupRequest := &dto.SignupRequest{}
-	bindReqErr := errors.New("Binding request failed")
 	errResponse := dto.ResponseErr{
 		StatusCode: http.StatusBadRequest,
-		Message:    constant.BindingRequestErrorMessage + bindReqErr.Error(),
+		Message:    constant.BindingRequestErrorMessage + t.bindErr.Error(),
 		Data:       nil,
 	}
 
@@ -68,7 +85,7 @@ func (t *AuthHandlerTest) TestSignupBindFailed() {
 	validator := mock_validator.NewMockIDtoValidator(controller)
 	context := mock_router.NewMockIContext(controller)
 
-	context.EXPECT().Bind(signupRequest).Return(bindReqErr)
+	context.EXPECT().Bind(t.signupRequest).Return(t.bindErr)
 	context.EXPECT().JSON(http.StatusBadRequest, errResponse)
 
 	handler := NewHandler(authSvc, userSvc, validator)
@@ -77,19 +94,6 @@ func (t *AuthHandlerTest) TestSignupBindFailed() {
 }
 
 func (t *AuthHandlerTest) TestSignupValidateFailed() {
-	signupRequest := &dto.SignupRequest{}
-	validateErr := []*dto.BadReqErrResponse{
-		{
-			Message:     "BadRequestError1",
-			FailedField: "Field1",
-			Value:       nil,
-		},
-		{
-			Message:     "BadRequestError2",
-			FailedField: "Field2",
-			Value:       nil,
-		},
-	}
 	errResponse := dto.ResponseErr{
 		StatusCode: http.StatusBadRequest,
 		Message:    constant.InvalidRequestBodyMessage + "BadRequestError1, BadRequestError2",
@@ -103,8 +107,8 @@ func (t *AuthHandlerTest) TestSignupValidateFailed() {
 	validator := mock_validator.NewMockIDtoValidator(controller)
 	context := mock_router.NewMockIContext(controller)
 
-	context.EXPECT().Bind(signupRequest).Return(nil)
-	validator.EXPECT().Validate(signupRequest).Return(validateErr)
+	context.EXPECT().Bind(t.signupRequest).Return(nil)
+	validator.EXPECT().Validate(t.signupRequest).Return(t.validateErr)
 	context.EXPECT().JSON(http.StatusBadRequest, errResponse)
 
 	handler := NewHandler(authSvc, userSvc, validator)
@@ -113,7 +117,6 @@ func (t *AuthHandlerTest) TestSignupValidateFailed() {
 }
 
 func (t *AuthHandlerTest) TestSignupServiceError() {
-	signupRequest := &dto.SignupRequest{}
 	signupError := &dto.ResponseErr{
 		StatusCode: http.StatusInternalServerError,
 		Message:    "Some Exception",
@@ -127,9 +130,9 @@ func (t *AuthHandlerTest) TestSignupServiceError() {
 	validator := mock_validator.NewMockIDtoValidator(controller)
 	context := mock_router.NewMockIContext(controller)
 
-	context.EXPECT().Bind(signupRequest).Return(nil)
-	validator.EXPECT().Validate(signupRequest).Return(nil)
-	authSvc.EXPECT().Signup(signupRequest).Return(nil, signupError)
+	context.EXPECT().Bind(t.signupRequest).Return(nil)
+	validator.EXPECT().Validate(t.signupRequest).Return(nil)
+	authSvc.EXPECT().Signup(t.signupRequest).Return(nil, signupError)
 	context.EXPECT().JSON(http.StatusInternalServerError, signupError)
 
 	handler := NewHandler(authSvc, userSvc, validator)
