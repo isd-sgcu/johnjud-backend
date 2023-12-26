c06d8c6b51e8eb27f8655dd1a9985fcd6dc1ea8b
Gear <84141000+macgeargear@users.noreply.github.com>
2023-12-26 13:46:24 +0700

    fix: pet dto

--- src/app/dto/pet.dto.go
+++ src/app/dto/pet.dto.go
@@ -21,3 +21,46 @@ type PetDto struct {
 	Address      string     `json:"address"`
 	Contact      string     `json:"contact"`
 }
+
+type FindOnePetDto struct {
+	Id string `json:"id" validate:"required"`
+}
+type CreatePetDto struct {
+	Type         string     `json:"type" validate:"required"`
+	Species      string     `json:"species" validate:"required"`
+	Name         string     `json:"name" validate:"required"`
+	Birthdate    string     `json:"birthdate" validate:"required"`
+	Gender       pet.Gender `json:"gender" validate:"required" example:"male"`
+	Habit        string     `json:"habit" validate:"required"`
+	Caption      string     `json:"caption"`
+	Status       pet.Status `json:"status" validate:"required" example:"findhome"`
+	IsSterile    bool       `json:"is_sterile" validate:"required"`
+	IsVaccinated bool       `json:"is_vaccine" validate:"required"`
+	IsVisible    bool       `json:"is_visible" validate:"required"`
+	IsClubPet    bool       `json:"is_club_pet" validate:"required"`
+	Background   string     `json:"background"`
+	Address      string     `json:"address"`
+	Contact      string     `json:"contact"`
+}
+
+type UpDatePetDto struct {
+	Type         string     `json:"type" validate:"required"`
+	Species      string     `json:"species" validate:"required"`
+	Name         string     `json:"name" validate:"required"`
+	Birthdate    string     `json:"birthdate" validate:"required"`
+	Gender       pet.Gender `json:"gender" validate:"required" example:"male"`
+	Habit        string     `json:"habit" validate:"required"`
+	Caption      string     `json:"caption"`
+	Status       pet.Status `json:"status" validate:"required" example:"findhome"`
+	IsSterile    bool       `json:"is_sterile" validate:"required"`
+	IsVaccinated bool       `json:"is_vaccine" validate:"required"`
+	IsVisible    bool       `json:"is_visible" validate:"required"`
+	IsClubPet    bool       `json:"is_club_pet" validate:"required"`
+	Background   string     `json:"background"`
+	Address      string     `json:"address"`
+	Contact      string     `json:"contact"`
+}
+
+type DeleteRequest struct {
+	Id string `json:"id" validate:"required"`
+}
