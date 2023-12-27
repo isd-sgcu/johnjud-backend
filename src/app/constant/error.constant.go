8fd984159ac816d80d4d246ed857ec962114309a
Gear <84141000+macgeargear@users.noreply.github.com>
2023-12-28 03:04:35 +0700

    feat: add setup test on pet service

--- src/app/constant/error.constant.go
+++ src/app/constant/error.constant.go
@@ -5,3 +5,6 @@ const InvalidRequestBodyMessage = "Invalid Request Body due to:"
 const DuplicateEmailMessage = "Duplicate Email"
 const InternalErrorMessage = "Internal Error"
 const UnavailableServiceMessage = "Unavailable Service"
+
+const ServiceDownMessage = "Service is down"
+const PetNotFoundMessage = "Pet not found"
--- src/app/service/pet/pet.service_test.go
+++ src/app/service/pet/pet.service_test.go
@@ -1,15 +1,22 @@
 package pet
 
 import (
+	"math/rand"
+	"net/http"
 	"testing"
 
+	"github.com/go-faker/faker/v4"
+	petConst "github.com/isd-sgcu/johnjud-backend/src/constant/pet"
+	"github.com/isd-sgcu/johnjud-gateway/src/app/constant"
 	"github.com/isd-sgcu/johnjud-gateway/src/app/dto"
+	image_proto "github.com/isd-sgcu/johnjud-go-proto/johnjub/file/image/v1"
 	proto "github.com/isd-sgcu/johnjud-go-proto/johnjud/backend/pet/v1"
 	"github.com/stretchr/testify/suite"
 )
 
 type PetServiceTest struct {
 	suite.Suite
+	Pets           []*proto.Pet
 	Pet            *proto.Pet
 	PetReq         *proto.CreatePetRequest
 	UpdatePetReq   *proto.UpdatePetRequest
@@ -17,13 +24,134 @@ type PetServiceTest struct {
 	NotFoundErr    *dto.ResponseErr
 	ServiceDownErr *dto.ResponseErr
 	InternalErr    *dto.ResponseErr
+
+	Images        []*image_proto.Image
+	ImageUrls     []string
+	ImagesList    [][]*image_proto.Image
+	ImageUrlsList [][]string
 }
 
 func TestPetService(t *testing.T) {
 	suite.Run(t, new(PetServiceTest))
 }
 
-func (t *PetServiceTest) SetupTest() {}
+func (t *PetServiceTest) SetupTest() {
+	var pets []*proto.Pet
+	for i := 0; i <= 3; i++ {
+		pet := &proto.Pet{
+			Id:           faker.UUIDDigit(),
+			Type:         faker.Word(),
+			Species:      faker.Word(),
+			Name:         faker.Name(),
+			Birthdate:    faker.Word(),
+			Gender:       petConst.Gender(rand.Intn(1) + 1),
+			Habit:        faker.Paragraph(),
+			Caption:      faker.Paragraph(),
+			Status:       petConst.Status(rand.Intn(1) + 1),
+			IsSterile:    true,
+			IsVaccinated: true,
+			IsVisible:    true,
+			IsClubPet:    true,
+			Background:   faker.Paragraph(),
+			Address:      faker.Paragraph(),
+			Contact:      faker.Paragraph(),
+		}
+		var images []*image_proto.Image
+		var imageUrls []string
+		for i := 0; i < 3; i++ {
+			url := faker.URL()
+			images = append(images, &image_proto.Image{
+				Id:       faker.UUIDDigit(),
+				PetId:    pet.Id,
+				ImageUrl: url,
+			})
+			imageUrls = append(imageUrls, url)
+		}
+		t.ImagesList = append(t.ImagesList, images)
+		t.ImageUrlsList = append(t.ImageUrlsList, imageUrls)
+		pets = append(pets, pet)
+	}
+
+	t.Pets = pets
+	t.Pet = t.Pets[0]
+
+	t.PetReq = &proto.CreatePetRequest{
+		Pet: &proto.Pet{
+			Type:         faker.Word(),
+			Species:      faker.Word(),
+			Name:         faker.Name(),
+			Birthdate:    faker.Word(),
+			Gender:       petConst.Gender(rand.Intn(1) + 1),
+			Habit:        faker.Paragraph(),
+			Caption:      faker.Paragraph(),
+			Status:       petConst.Status(rand.Intn(1) + 1),
+			IsSterile:    true,
+			IsVaccinated: true,
+			IsVisible:    true,
+			IsClubPet:    true,
+			Background:   faker.Paragraph(),
+			Address:      faker.Paragraph(),
+			Contact:      faker.Paragraph(),
+		},
+	}
+
+	t.PetDto = &dto.PetDto{
+		Type:         t.Pet.Type,
+		Species:      t.Pet.Species,
+		Name:         t.Pet.Name,
+		Birthdate:    t.Pet.Birthdate,
+		Gender:       petConst.Gender(t.Pet.Gender),
+		Habit:        t.Pet.Habit,
+		Caption:      t.Pet.Caption,
+		Status:       petConst.Status(t.Pet.Status),
+		IsSterile:    t.Pet.IsSterile,
+		IsVaccinated: t.Pet.IsVaccinated,
+		IsVisible:    t.Pet.IsVisible,
+		IsClubPet:    t.Pet.IsClubPet,
+		Background:   t.Pet.Background,
+		Address:      t.Pet.Address,
+		Contact:      t.Pet.Contact,
+	}
+
+	t.UpdatePetReq = &proto.UpdatePetRequest{
+		Pet: &proto.Pet{
+			Id:           t.Pet.Id,
+			Type:         t.Pet.Type,
+			Species:      t.Pet.Species,
+			Name:         t.Pet.Name,
+			Birthdate:    t.Pet.Birthdate,
+			Gender:       petConst.Gender(t.Pet.Gender),
+			Habit:        t.Pet.Habit,
+			Caption:      t.Pet.Caption,
+			Status:       petConst.Status(t.Pet.Status),
+			IsSterile:    t.Pet.IsSterile,
+			IsVaccinated: t.Pet.IsVaccinated,
+			IsVisible:    t.Pet.IsVisible,
+			IsClubPet:    t.Pet.IsClubPet,
+			Background:   t.Pet.Background,
+			Address:      t.Pet.Address,
+			Contact:      t.Pet.Contact,
+		},
+	}
+
+	t.ServiceDownErr = &dto.ResponseErr{
+		StatusCode: http.StatusServiceUnavailable,
+		Message:    constant.ServiceDownMessage,
+		Data:       nil,
+	}
+
+	t.NotFoundErr = &dto.ResponseErr{
+		StatusCode: http.StatusServiceUnavailable,
+		Message:    constant.PetNotFoundMessage,
+		Data:       nil,
+	}
+
+	t.InternalErr = &dto.ResponseErr{
+		StatusCode: http.StatusServiceUnavailable,
+		Message:    constant.InternalErrorMessage,
+		Data:       nil,
+	}
+}
 
 func (t *PetServiceTest) TestFindAllSuccess() {}
 
