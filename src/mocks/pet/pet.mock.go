f4e862ba3db72b26de68bcc74468d12446a4d585
Gear <84141000+macgeargear@users.noreply.github.com>
2023-12-28 03:11:51 +0700

    feat: add Create pet mock

--- src/mocks/pet/pet.mock.go
+++ src/mocks/pet/pet.mock.go
@@ -23,3 +23,17 @@ func (s *ServiceMock) FindOne(id string) (result *proto.Pet, err *dto.ResponseEr
 
 	return
 }
+
+func (s *ServiceMock) Create(in *dto.PetDto) (result *proto.Pet, err *dto.ResponseErr) {
+	args := s.Called(in)
+
+	if args.Get(0) != nil {
+		result = args.Get(0).(*proto.Pet)
+	}
+
+	if args.Get(1) != nil {
+		err = args.Get(1).(*dto.ResponseErr)
+	}
+
+	return
+}
