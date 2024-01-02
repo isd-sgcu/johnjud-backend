79f48f057c8b5d1374b3fbc268f3d106004b0701
Gear <84141000+macgeargear@users.noreply.github.com>
2024-01-02 17:46:23 +0700

    feat: custom unmarshalJSON for `Status` and `Gender`

--- src/constant/pet/pet.constant.go
+++ src/constant/pet/pet.constant.go
@@ -1,5 +1,11 @@
 package pet
 
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+)
+
 type Gender int
 
 const (
@@ -13,3 +19,41 @@ const (
 	ADOPTED  = 1
 	FINDHOME = 2
 )
+
+func (g *Gender) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	s = strings.ToUpper(s)
+	switch s {
+	case "MALE":
+		*g = Gender(1)
+		return nil
+	case "FEMALE":
+		*g = Gender(2)
+		return nil
+
+	default:
+		return errors.New("invalid gender")
+	}
+}
+
+func (st *Status) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	s = strings.ToUpper(s)
+	switch s {
+	case "ADOPTED":
+		*st = Status(1)
+		return nil
+	case "FINDHOME":
+		*st = Status(2)
+		return nil
+
+	default:
+		return errors.New("invalid gender")
+	}
+}
