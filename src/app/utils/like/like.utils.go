eae566cd3596a21ce42acd5726fc47814d123620
Gear <84141000+macgeargear@users.noreply.github.com>
2024-01-06 00:58:30 +0700

    feat: like utils

---
+++ src/app/utils/like/like.utils.go
@@ -0,0 +1,33 @@
+package like
+
+import (
+	"github.com/isd-sgcu/johnjud-gateway/src/app/dto"
+	likeProto "github.com/isd-sgcu/johnjud-go-proto/johnjud/backend/like/v1"
+)
+
+func ProtoToDto(in *likeProto.Like) *dto.LikeResponse {
+	return &dto.LikeResponse{
+		UserID: in.UserId,
+		PetID:  in.PetId,
+	}
+}
+
+func ProtoToDtoList(in []*likeProto.Like) []*dto.LikeResponse {
+	var res []*dto.LikeResponse
+	for _, i := range in {
+		res = append(res, &dto.LikeResponse{
+			UserID: i.UserId,
+			PetID:  i.PetId,
+		})
+	}
+	return res
+}
+
+func CreateDtoToProto(in *dto.CreateLikeRequest) *likeProto.CreateLikeRequest {
+	return &likeProto.CreateLikeRequest{
+		Like: &likeProto.Like{
+			UserId: in.UserID,
+			PetId:  in.PetID,
+		},
+	}
+}
