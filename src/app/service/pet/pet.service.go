ce0e52f094ba98f6f0d50f2e03934792f55286bd
Idhibhat Pankam <marginpankam@gmail.com>
2024-01-05 23:03:11 +0700

    fix: typo

--- src/app/service/pet/pet.service.go
+++ src/app/service/pet/pet.service.go
@@ -94,7 +94,7 @@ func (s *Service) FindOne(id string) (result *dto.PetResponse, err *dto.Response
 	return findOneResponse, nil
 }
 
-func (s *Service) Create(in *dto.CreatePetRequest) (ressult *dto.PetResponse, err *dto.ResponseErr) {
+func (s *Service) Create(in *dto.CreatePetRequest) (result *dto.PetResponse, err *dto.ResponseErr) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
