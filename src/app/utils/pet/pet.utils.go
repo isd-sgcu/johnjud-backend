dfbf892f5cec23ce103a54a5143300e8cb4aad75
Gear <84141000+macgeargear@users.noreply.github.com>
2024-01-05 17:13:58 +0700

    fix: remove mockImages use mockImagesList instead and use constant value instead of faker

--- src/app/utils/pet/pet.utils.go
+++ src/app/utils/pet/pet.utils.go
@@ -1,7 +1,8 @@
 package pet
 
 import (
-	"github.com/go-faker/faker/v4"
+	"fmt"
+
 	imgproto "github.com/isd-sgcu/johnjud-go-proto/johnjud/file/image/v1"
 )
 
@@ -11,27 +12,14 @@ func MockImageList(n int) [][]*imgproto.Image {
 		var images []*imgproto.Image
 		for j := 0; j <= 3; j++ {
 			images = append(images, &imgproto.Image{
-				Id:        faker.UUIDDigit(),
-				PetId:     faker.UUIDDigit(),
-				ImageUrl:  faker.URL(),
-				ObjectKey: faker.Word(),
+				Id:        fmt.Sprintf("%v%v", i, j),
+				PetId:     fmt.Sprintf("%v%v", i, j),
+				ImageUrl:  fmt.Sprintf("%v%v", i, j),
+				ObjectKey: fmt.Sprintf("%v%v", i, j),
 			})
 		}
 		imagesList = append(imagesList, images)
 	}
 
 	return imagesList
-}
-
-func MockImages() []*imgproto.Image {
-	var images []*imgproto.Image
-	for j := 0; j <= 3; j++ {
-		images = append(images, &imgproto.Image{
-			Id:        faker.UUIDDigit(),
-			PetId:     faker.UUIDDigit(),
-			ImageUrl:  faker.URL(),
-			ObjectKey: faker.Word(),
-		})
-	}
-	return images
 }
