b625e8d33dc384b749cda2face6b1c0e067ad648
Idhibhat Pankam <marginpankam@gmail.com>
2024-01-09 23:24:30 +0700

    feat: add admin paths

--- src/constant/auth/auth.constant.go
+++ src/constant/auth/auth.constant.go
@@ -4,11 +4,18 @@ var ExcludePath = map[string]struct{}{
 	"POST /auth/signup": {},
 	"POST /auth/signin": {},
 	"POST /auth/verify": {},
-	"GET /pet/":         {},
+	"GET /pets/":        {},
 	"GET /adopt/":       {},
 }
 
-var AdminPath = map[string]struct{}{}
+var AdminPath = map[string]struct{}{
+	"DELETE /users/:id":     {},
+	"POST /pets/":           {},
+	"PUT /pets/:id":         {},
+	"PUT /pets/:id/visible": {},
+	"DELETE /pets/:id":      {},
+	//need to add image upload, delete, assignpet
+}
 
 var VersionList = map[string]struct{}{
 	"v1": {},
