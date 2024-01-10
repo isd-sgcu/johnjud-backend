4b3f1ee69434f10690c5f0161f2e24aacfcc79d7
Gear <84141000+macgeargear@users.noreply.github.com>
2024-01-10 17:04:31 +0700

    chore

--- src/app/dto/image.dto.go
+++ src/app/dto/image.dto.go
@@ -1,5 +1,11 @@
 package dto
 
+type ImageResponse struct {
+	Id        string `json:"id"`
+	Url       string `json:"url"`
+	ObjectKey string `json:"object_key"`
+}
+
 type UploadImageRequest struct {
 	Filename string `json:"filename" validate:"required"`
 	Data     []byte `json:"data" validate:"required"`
