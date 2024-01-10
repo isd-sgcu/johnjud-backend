e5a40f30f74b4f01976d7ee8ad324e6d2e28f93d
Gear <84141000+macgeargear@users.noreply.github.com>
2024-01-10 15:51:06 +0700

    chore

---
+++ src/constant/image/image.constant.go
@@ -0,0 +1,5 @@
+package image
+
+const FindImageSuccessMessage = "find images success"
+const UploadImageSuccessMessage = "upload image success"
+const DelteImageSuccessMessage = "delete image success"
