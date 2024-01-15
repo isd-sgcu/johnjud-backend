9d0efb5e6106656c245740391d338d86170ccb65
NitiwatOwen <nitiwat3478@gmail.com>
2024-01-15 17:02:57 +0700

    feat: add reset-password route

--- src/app/router/auth.router.go
+++ src/app/router/auth.router.go
@@ -8,3 +8,10 @@ func (r *FiberRouter) PostAuth(path string, h func(ctx IContext)) {
 		return nil
 	})
 }
+
+func (r *FiberRouter) PutAuth(path string, h func(ctx IContext)) {
+	r.auth.Put(path, func(c *fiber.Ctx) error {
+		h(NewFiberCtx(c))
+		return nil
+	})
+}
