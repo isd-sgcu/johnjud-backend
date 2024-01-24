e7e85522c2076fcb23b01fd34000ee45b0efc5b5
Idhibhat Pankam <marginpankam@gmail.com>
2024-01-24 22:00:37 +0700

    temp: disable admin paths

--- src/constant/auth/auth.constant.go
+++ src/constant/auth/auth.constant.go
@@ -12,15 +12,15 @@ var ExcludePath = map[string]struct{}{
 }
 
 var AdminPath = map[string]struct{}{
-	"DELETE /user/:id":            {},
-	"POST /pets":                  {},
-	"PUT /pets/:id":               {},
-	"PUT /pets/:id/visible":       {},
-	"DELETE /pets/:id":            {},
-	"POST /images/assign/:pet_id": {},
-	"DELETE /images/:id":          {},
-	"POST /images/":               {},
-	"GET /images/:id":             {},
+	// "DELETE /user/:id":            {},
+	// "POST /pets":                  {},
+	// "PUT /pets/:id":               {},
+	// "PUT /pets/:id/visible":       {},
+	// "DELETE /pets/:id":            {},
+	// "POST /images/assign/:pet_id": {},
+	// "DELETE /images/:id":          {},
+	// "POST /images/":               {},
+	// "GET /images/:id":             {},
 }
 
 var VersionList = map[string]struct{}{
