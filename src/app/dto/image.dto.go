ff4f68a390fb01d6a593c73333719150e12281b3
Idhibhat Pankam <marginpankam@gmail.com>
2024-01-29 22:57:39 +0700

    fix: image upload petId not required

--- src/app/dto/image.dto.go
+++ src/app/dto/image.dto.go
@@ -9,7 +9,7 @@ type ImageResponse struct {
 type UploadImageRequest struct {
 	Filename string `json:"filename" validate:"required"`
 	Data     []byte `json:"data" validate:"required"`
-	PetId    string `json:"pet_id" validate:"required"`
+	PetId    string `json:"pet_id"`
 }
 
 type DeleteImageResponse struct {
