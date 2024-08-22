5b35bc627c9f1ab6467e37fa910e36efc3e36fce
Idhibhat Pankam <marginpankam@gmail.com>
2024-08-22 22:08:34 +0700

    model

--- constant/error.constant.go
+++ constant/error.constant.go
@@ -26,3 +26,14 @@ const DuplicateEmailErrorMessage = "Duplicate email"
 const InternalServerErrorMessage = "Internal server error"
 
 const UserNotFoundErrorMessage = "User not found"
+
+// file
+const UploadToBucketErrorMessage = "Error uploading to bucket client"
+const DeleteFromBucketErrorMessage = "Error deleting from bucket client"
+
+const ImageNotFoundErrorMessage = "Image not found"
+const CreateImageErrorMessage = "Error creating image in db"
+const DeleteImageErrorMessage = "Error deleting image from db"
+const PrimaryKeyRequiredErrorMessage = "UUID Primary key (petId) required"
+const PetIdNotUUIDErrorMessage = "Pet id is not uuid"
+const PetIdNotFoundErrorMessage = "Pet id not found"
